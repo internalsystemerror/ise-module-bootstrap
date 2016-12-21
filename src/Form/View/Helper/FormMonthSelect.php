db26afcf90778551354cf893c4852047fa715a06
ise <iam@ise.me.uk>
2016-12-21 13:20:08 +0000

    Fix class name

--- src/Form/View/Helper/FormMonthSelect.php
+++ src/Form/View/Helper/FormMonthSelect.php
@@ -4,7 +4,7 @@ namespace IseBootstrap\Form\View\Helper;
 
 use Zend\Form\View\Helper\FormInput as FormInputHelper;
 
-class FormInput extends FormInputHelper
+class FormMonthSelect extends FormInputHelper
 {
 
     use FormInputTrait;
