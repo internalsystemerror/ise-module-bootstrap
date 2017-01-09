da14dd9ad7f3810e7f2c5745ed7fcde1be9aab60
ise <iam@ise.me.uk>
2017-01-09 19:54:38 +0000

    Fixes to factories for ZF2+3 comppatibility

--- src/Factory/DispatchListenerFactory.php
+++ src/Factory/DispatchListenerFactory.php
@@ -19,8 +19,7 @@ class DispatchListenerFactory implements FactoryInterface
      */
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        $viewRenderer = $container->get('ViewRenderer');
-        return new $requestedName($viewRenderer);
+        return new $requestedName($container->get('ViewRenderer'));
     }
     
     /**
