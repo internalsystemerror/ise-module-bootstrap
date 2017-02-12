ed2405c778419981b24ed59937d1ea76b20928ee
ise <iam@ise.me.uk>
2017-02-12 13:34:48 +0000

    Add test

--- test/Form/View/Helper/FormButtonTest.php
+++ test/Form/View/Helper/FormButtonTest.php
@@ -3,44 +3,28 @@
 namespace IseTest\Bootstrap\Form\View\Helper;
 
 use IseBootstrap\Form\View\Helper\FormButton;
+use ZendTest\Form\View\Helper\CommonTestCase;
+use ZendTest\Form\View\Helper\FormButtonTest as ZendFormButtonTest;
 
-class FormButtonTest extends \PHPUnit_Framework_TestCase
+class FormButtonTest extends ZendFormButtonTest
 {
-
-    /**
-     * @var FormButton
-     */
-    protected $object;
 
     /**
      * Sets up the fixture
      */
     protected function setUp()
     {
-        $this->object = new FormButton;
+        $this->helper = new FormButton;
+        CommonTestCase::setUp();
     }
-
+    
     /**
-     * @covers IseBootstrap\Form\View\Helper\FormButton::render
-     * @todo   Implement testRender().
+     * {@inheritDoc}
      */
-    public function testRender()
+    public function validAttributes()
     {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
-
-    /**
-     * @covers IseBootstrap\Form\View\Helper\FormButton::getIconHelper
-     * @todo   Implement testGetIconHelper().
-     */
-    public function testGetIconHelper()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        return [
+            ['href', 'assertContains'],
+        ];
     }
 }
