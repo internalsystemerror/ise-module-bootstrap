0c049550124e847b0aee6fcee3d720b74d516e55
ise <iam@ise.me.uk>
2017-02-15 19:30:48 +0000

    Refactor JS

--- assets/js/bootstrap-global.js
+++ assets/js/bootstrap-global.js
@@ -1,21 +1,53 @@
-(function ($)
-{
-    var
-    constants = {
-    },
-    properties = {
-    },
-    methods = {
-        init: function ()
-        {
-            $('time.timeago').timeago();
-            $('.modal').modal().on('shown.bs.modal', methods.modalShown);
-        },
-        modalShown: function ()
-        {
-            $(this).focus();
-        }
+(function ($, document, window, undefined) {
+    
+    'use strict';
+    
+    var $document = $(document), $window = $(window), eventNames = {
+        ready: 'ise:ready',
+        load: 'ise:load'
+    }, selectors = {
+        time: '.timeago',
+        modal: '.modal'
     };
-
-    $(document).ready(methods.init);
-})(jQuery);
+    
+    /**
+     * Initialise
+     */
+    function initialise() {
+        $document.ready(documentReady).on(eventNames.ready, iseReady);
+        $window.load(windowLoad);
+    }
+    
+    /**
+     * Register document ready event
+     */
+    function documentReady() {
+        $document.trigger(eventNames.ready);
+    }
+    
+    /**
+     * Register window load event
+     */
+    function windowLoad() {
+        $window.trigger(eventNames.load);
+    }
+    
+    /**
+     * Custom ready event
+     */
+    function iseReady() {
+        $(selectors.time).timeago();
+        $(selectors.modal).modal().on('shown.bs.modal', modalShown);
+    }
+    
+    /**
+     * Modal is shown
+     */
+    function modalShown() {
+        $(this).focus();
+    }
+    
+    // Initialise
+    initialise();
+    
+})(jQuery, document, window);
