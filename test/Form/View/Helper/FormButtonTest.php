e27ac482abf18e3eec8718c789f3175deb41fe80
ise <iam@ise.me.uk>
2017-03-01 10:58:20 +0000

    Merge

--- test/Form/View/Helper/FormButtonTest.php
+++ test/Form/View/Helper/FormButtonTest.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 
 namespace IseTest\Bootstrap\Form\View\Helper;
@@ -33,3 +34,35 @@ class FormButtonTest extends ZendFormButtonTest
         ];
     }
 }
+=======
+<?php
+
+namespace IseTest\Bootstrap\Form\View\Helper;
+
+use Ise\Bootstrap\Form\View\Helper\FormButton;
+use ZendTest\Form\View\Helper\CommonTestCase;
+use ZendTest\Form\View\Helper\FormButtonTest as ZendFormButtonTest;
+
+class FormButtonTest extends ZendFormButtonTest
+{
+
+    /**
+     * Sets up the fixture
+     */
+    protected function setUp()
+    {
+        $this->helper = new FormButton;
+        CommonTestCase::setUp();
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function validAttributes()
+    {
+        return [
+            ['href', 'assertContains'],
+        ];
+    }
+}
+>>>>>>> Stashed changes
