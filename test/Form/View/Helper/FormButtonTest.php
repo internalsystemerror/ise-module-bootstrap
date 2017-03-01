5d8876ec2a5917c8464f42ff33c218eac4b50474
ise <iam@ise.me.uk>
2017-03-01 10:59:14 +0000

    Fix conflict

--- test/Form/View/Helper/FormButtonTest.php
+++ test/Form/View/Helper/FormButtonTest.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <?php
 
 namespace IseTest\Bootstrap\Form\View\Helper;
@@ -34,35 +33,3 @@ class FormButtonTest extends ZendFormButtonTest
         ];
     }
 }
-=======
-<?php
-
-namespace IseTest\Bootstrap\Form\View\Helper;
-
-use Ise\Bootstrap\Form\View\Helper\FormButton;
-use ZendTest\Form\View\Helper\CommonTestCase;
-use ZendTest\Form\View\Helper\FormButtonTest as ZendFormButtonTest;
-
-class FormButtonTest extends ZendFormButtonTest
-{
-
-    /**
-     * Sets up the fixture
-     */
-    protected function setUp()
-    {
-        $this->helper = new FormButton;
-        CommonTestCase::setUp();
-    }
-    
-    /**
-     * {@inheritDoc}
-     */
-    public function validAttributes()
-    {
-        return [
-            ['href', 'assertContains'],
-        ];
-    }
-}
->>>>>>> Stashed changes
